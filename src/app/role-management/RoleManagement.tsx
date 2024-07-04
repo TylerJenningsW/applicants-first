23b56ff968ad7d63fc0feb2522e2df504b92ae0a
yoendris97 <yrodriguez2@student.fullsail.edu>
2024-07-04 11:57:35 -0400

    role management UI implemented

---
+++ src/app/role-management/RoleManagement.tsx
@@ -0,0 +1,48 @@
+"use client"; // Add this directive
+
+import React, {useEffect} from 'react';
+import Image from 'next/image';
+
+
+import '../styles/RoleManagement.css'; // Import the CSS file
+
+const RoleManagement: React.FC = () => {
+
+  return (
+    <div className="container1">
+      
+      
+      <header className="header1">
+        <div className="logo1">
+          <Image src="/images/Logo.png" alt="App Logo" width={40} height={40} />
+          <h1>Applicants First</h1>
+        </div>
+      </header>
+      
+      <main className="main1">
+        
+        <Image src="/images/Logo.png" alt="Applicants First" width={80} height={80} />
+        
+        <h2>Select your Role</h2>
+
+        <div className="dropdownGroup">
+          <select className="dropdown">
+            <option value="" disabled>Select a Role</option>
+            <option value="Administrator">Administrator</option>
+            <option value="Recruiter">Recruiter</option>
+            <option value="Client">Client</option>
+          </select>
+        </div>
+        
+        <button className="button1">
+          Continue
+        </button>
+       
+      
+      </main>
+      
+    </div>
+  );
+};
+
+export default RoleManagement;
---
+++ src/app/role-management/page.tsx
@@ -0,0 +1,11 @@
+"use client"; // Add this directive if using Next.js
+
+
+import React from 'react';
+import RoleManagement from './RoleManagement';
+
+const RoleManagementPage: React.FC = () => {
+  return <RoleManagement />;
+};
+
+export default RoleManagementPage;
