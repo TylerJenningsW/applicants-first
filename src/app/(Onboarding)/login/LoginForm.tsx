d3e1a04fae9abd3f152ef60842b4904597d086f0
Tyler Jennings <tylerjennings2@outlook.com>
2024-07-07 13:56:59 -0400

    fixed style

--- src/app/(Onboarding)/login/LoginForm.tsx
+++ src/app/(Onboarding)/login/LoginForm.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import '@/app/styles/Login.css'
+import '../../styles/Login.css'
 import Link from 'next/link'
 import { login } from './actions' // Import the actions
 
