f37c43c30fd603ab86f7afe5060b1c6befb9b95d
yoendris97 <yrodriguez2@student.fullsail.edu>
2024-08-13 16:36:04 -0400

    Added filter feature  to search for a spacific job

--- src/app/jobs/actions.ts
+++ src/app/jobs/actions.ts
@@ -1,16 +1,41 @@
-'use server'
-import prisma from '../../../utils/prisma/prismaClient'
+// 'use server'
+// import prisma from '../../../utils/prisma/prismaClient'
 
-export default async function fetchJobs() {
+// export default async function fetchJobs() {
+//   try {
+//     const jobs = await prisma.job.findMany({
+//       include: {
+//         organization: true,
+//       },
+//     })
+//     return jobs
+//   } catch (error) {
+//     console.error('Error fetching jobs:', error)
+//     return []
+//   }
+// }
+
+'use server';
+import prisma from '../../../utils/prisma/prismaClient';
+
+export default async function fetchJobs(searchTerm = '') {
   try {
     const jobs = await prisma.job.findMany({
+      where: searchTerm
+        ? {
+            JobTitle: {
+              contains: searchTerm,
+              mode: 'insensitive',
+            },
+          }
+        : {},
       include: {
         organization: true,
       },
-    })
-    return jobs
+    });
+    return jobs;
   } catch (error) {
-    console.error('Error fetching jobs:', error)
-    return []
+    console.error('Error fetching jobs:', error);
+    return [];
   }
 }
