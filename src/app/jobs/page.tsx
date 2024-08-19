eebef079bd2840addd1752e42335e48996b977f8
yoendris97 <yrodriguez2@student.fullsail.edu>
2024-08-19 19:17:47 -0400

    fixed bug

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -83,7 +83,7 @@ const JobsPage = () => {
               </div>
             ))
           ) : (
-            <p>No jobs found for "{searchTerm}"</p>
+            <p> No jobs found for &apos;{searchTerm}&apos; </p>
           )}
         </div>
       )}
