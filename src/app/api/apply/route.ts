018d709a9daf051066ecfb4813c0bdefbf4d67de
Tyler Jennings <tyler@docuguardian.com>
2024-08-25 15:31:01 -0400

    feat: Added resume parser backend logic

--- src/app/api/apply/route.ts
+++ src/app/api/apply/route.ts
@@ -1,12 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server'
 import prisma from '../../../../utils/prisma/prismaClient'
 import { createClient } from '../../../../utils/supabase/server'
-import { CookieOptions, createServerClient } from '@supabase/ssr';
+import { parseResumeFromPdf } from '../../../../utils/parser';
+import { Prisma } from '@prisma/client';
+
 // Only allow PDF and DOCX files
 const allowedMimeTypes = [
   'application/pdf',
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 ];
+
 export async function POST(req: NextRequest) {
   const supabase = createClient()
 
@@ -34,19 +37,30 @@ export async function POST(req: NextRequest) {
 
     const resumeUrl = supabase.storage.from('resumes').getPublicUrl(data.path).data.publicUrl
 
+    // Parse the resume
+    let parsedResume: Prisma.JsonValue | undefined;
+    if (file.type === 'application/pdf') {
+      parsedResume = await parseResumeFromPdf(resumeUrl)
+    } else {
+      // For DOCX files, you might need a different parsing function
+      // parsedResume = await parseResumeFromDocx(resumeUrl)
+      console.log('DOCX parsing not implemented yet')
+    }
+
     // Create applicant record
     const applicant = await prisma.applicant.create({
       data: {
         fullname: formData.get('fullname') as string,
         emailaddress: formData.get('emailaddress') as string,
-        alternateemailaddress: formData.get('alternateemailaddress') as string,
-        streetaddress: formData.get('streetaddress') as string,
-        city: formData.get('city') as string,
-        state: formData.get('state') as string,
-        zipcode: formData.get('zipcode') as string,
-        country: formData.get('country') as string,
-        linkedinurl: formData.get('linkedinurl') as string,
+        alternateemailaddress: formData.get('alternateemailaddress') as string || null,
+        streetaddress: formData.get('streetaddress') as string || null,
+        city: formData.get('city') as string || null,
+        state: formData.get('state') as string || null,
+        zipcode: formData.get('zipcode') as string || null,
+        country: formData.get('country') as string || null,
+        linkedinurl: formData.get('linkedinurl') as string || null,
         resumeUrl: resumeUrl,
+        parsedResume: parsedResume || Prisma.JsonNull, // hack to avoid null error
         job_id: parseInt(jobId, 10),
       },
     })
